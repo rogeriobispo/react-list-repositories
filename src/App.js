ebf989fe83e822ad293d1d0084b4b40f16a8e9fa
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2020-04-26 22:33:27 -0300

    chalenge done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
+import { uuid } from 'uuidv4'
+import Api from './services/api'
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories] = useState([])
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+  useEffect(() => {
 
+    const loadRepositories = async () => {
+      const response = await Api.get('/repositories')
+      setRepositories(response.data)
+    }
+
+    loadRepositories()
+  }, [])
+  const handleAddRepository = useCallback(
+    async () => {
+      const response = await Api.post('/repositories', {
+        id: uuid(),
+        url: "https://github.com/josepholiveira",
+        title: `Desafio ReactJS ${new Date()} `,
+        techs: ["React", "Node.js"],
+      })
+      setRepositories([...repositories, response.data])
+    },
+    [repositories],
+  )
+
+  const handleRemoveRepository = useCallback(
+    async (id) => {
+      await Api.delete(`/repositories/${id}`)
+      const filteredRepositories = repositories.filter(repo => repo.id !== id)
+      await setRepositories(filteredRepositories)
+
+
+    },
+    [repositories],
+  )
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
---
+++ src/readme.md
@@ -0,0 +1,15 @@
+##  desafio :rocket: RocketSeat :rocket:
+
+React simple front repositories
+
+## Funcionalidades 
+- lista repositórios 
+- remove repositório
+  
+## Tecnologias.
+- node
+- express
+- typescript
+- eslint
+- prettier
+
